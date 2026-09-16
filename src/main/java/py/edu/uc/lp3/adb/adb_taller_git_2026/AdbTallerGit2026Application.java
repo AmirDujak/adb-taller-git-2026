831f4707564841447312a29b013caec76971298f
Amir Dujak <amirdujak020@gmail.com>
2026-09-16 14:11:14 -0300

    chore: esqueleto Spring Boot API REST desde start.spring.io

---
+++ src/main/java/py/edu/uc/lp3/adb/adb_taller_git_2026/AdbTallerGit2026Application.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3.adb.adb_taller_git_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdbTallerGit2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdbTallerGit2026Application.class, args);
+	}
+
+}
